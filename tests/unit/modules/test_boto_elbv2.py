6e17421ad4e1d10cefd243e66ec6a185687afa04
Twangboy <leesh@vmware.com>
2022-03-29 16:33:17 -0600

    Fix unit tests

--- tests/unit/modules/test_boto_elbv2.py
+++ tests/unit/modules/test_boto_elbv2.py
@@ -1,23 +1,27 @@
-import salt.modules.boto_elbv2 as boto_elbv2
-from tests.support.mock import patch
-from tests.support.unit import TestCase
+import pytest
+from salt.modules import boto_elbv2
+from tests.support.mock import MagicMock, patch
 
 
-class BotoElbv2TestCase(TestCase):
-    """
-    TestCase for salt.modules.boto_elbv2 module
-    """
+@pytest.fixture
+def configure_loader_modules():
 
-    @patch("salt.utils.versions.check_boto_reqs")
-    @patch("salt.utils.boto3mod.assign_funcs")
-    def test___virtual__has_boto_reqs_true(self, mock_assign_funcs, mock_boto_reqs):
-        mock_boto_reqs.return_value = True
-        mock_assign_funcs.return_value = False
+    return {
+        boto_elbv2: {
+            "__utils__": {
+                "boto3.assign_funcs": MagicMock(),
+            },
+        }
+    }
+
+
+def test___virtual_has_boto_reqs_true():
+    with patch("salt.utils.versions.check_boto_reqs", return_value=True):
         result = boto_elbv2.__virtual__()
-        self.assertEqual(result, True)
+    assert result is True
 
-    @patch("salt.utils.versions.check_boto_reqs")
-    def test___virtual__has_boto_reqs_false(self, mock_boto_reqs):
-        mock_boto_reqs.return_value = False
+
+def test___virtual_has_boto_reqs_false():
+    with patch("salt.utils.versions.check_boto_reqs", return_value=False):
         result = boto_elbv2.__virtual__()
-        self.assertEqual(result, False)
+    assert result is False
